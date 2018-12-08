62ac8c052a88ce3fc8f77f45394ebb7f35777306
Indy <kdevlinmurray@gmail.com>
2018-12-07 23:17:38 -0800

    fixed julia algo, added comments, needs to be clean and norm

--- src/main.c
+++ src/main.c
@@ -58,48 +58,64 @@ void	image_put(t_fractal *fractal)
 
 void	img_pixel_put(t_img *img, int x, int y, int color)
 {
-	VAR(int, i, 0);
+	int i = 0;
+
 	i = (x + y * img->size_line / sizeof(int));
 	img->pixels[i] = color;
 }
 
+
 t_fractal		*init_fractal(char *title)
 {
-	VAR(t_fractal*, fractal, (t_fractal *)ft_memalloc(sizeof(t_fractal)));
+	// Allocates memory the size of a t_fractal struct that contains a
+	// void *mlx, void *win, and a pointer to the t_img struct, which 
+	// contains a void *ptr, and int array of pixels, an int for bit per pixel,
+	// and int for the size of the line, and an int that represents the endian
+	t_fractal* fractal = (t_fractal *)ft_memalloc(sizeof(t_fractal));
+
 	fractal->mlx = mlx_init();
 	fractal->win = mlx_new_window(fractal->mlx, WINX, WINY, title);
+	// could put protection here in case either prev function fails
 	image_init(fractal);
 	return (fractal);
 }
 
 void		build_fractal()
 {
-	VAR(t_fractal*, julia, init_fractal("Julia"));
-	VAR(int, x, -1);
-	VAR(int, y, -1);
-	VAR(int, i, 0);
-	VAR(long double, nr, 0);
-	VAR(long double, or, 0);
-	VAR(long double, ni, 0);
-	VAR(long double, oi, 0);
-	VAR(int, color, 0);
+	t_fractal* julia = init_fractal("Julia");
+	int x = -1;
+	int y = -1;
+	int i = 0;
+	long double zx = 0;
+	long double zy = 0;
+	int color = 0;
+	int max_iter = 256;
+	int move_x = 0;
+	int move_y = 0;
+	double c_x = -.7;
+	double c_y = .27015;
+	int zoom = 1;
+	long double tmp;
+	
 	while (++y < WINY)
 	{
 		x = -1;
 		while (++x < WINX)
 		{
-			nr = 1.5 * (x - WINX / 2) / (.5 * WINX);
-			ni = (y - WINY / 2) / (.5 * WINY);
-			while (++i < 500)
+			zx = 1.5 * (x - WINX / 2) / (.5 * zoom * WINX) + move_x;
+			zy = (y - WINY / 2) / (.5 * zoom * WINY) + move_y;
+			while (zx * zx + zy * zy < 4 && ++i < max_iter)
 			{
-				or = nr;
-				oi = ni;
-				nr = or * or + oi * oi + 0;
-				ni = 2 * or * oi - 0;
-				if ((nr * nr + ni * ni) > 4)
-					break ;
+				tmp = zx * zx - zy * zy + c_x;
+				zy = 2 * zx * zy + c_y;
+				zx = tmp;
+				// or = zx;
+				// oi = zy;
+				// zx = tmp * tmp + oi * oi + 0;
+				// zy = 2 * tmp * oi - 0;
 			}
-			color = ((i % 256) << 8 | (i * i % 256) << 16 | (300 - i) % 256);
+			// color = R | G | B
+			color = ((i % 256) << 16 | (i * i % 256) << 8 | (255 - i) % 256);
 			img_pixel_put(julia->img, x, y, color);
 			i = 0;
 		}
